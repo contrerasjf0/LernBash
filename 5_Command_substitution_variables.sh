de39980136e1d2b085c86cdb5a02cc4ba33b313d
Juan Francisco <contrerasjf.0@hotmail.com>
2021-02-01 13:08:28 -0600

    Command substitution in variables
    Resolve: C-09

---
+++ 5_Command_substitution_variables.sh
@@ -0,0 +1,12 @@
+# !/bin/bash
+
+
+# Coomand substitution in variables
+
+currentPlace= `pwd`
+infoKernel=$(uname -a)
+
+echo "The current directory is: $currentPlace"
+echo "Info Kernel: $infoKernel"
+
+
